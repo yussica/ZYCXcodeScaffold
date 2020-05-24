6b5924afc3f8bbf8aba60b18b90f0f5e1f19a36f
朱靥超 <yechao.zhu@dianrong.com>
2020-05-24 12:50:21 +0900

    update script

--- copy.sh
+++ copy.sh
@@ -3,11 +3,11 @@
 FILE="$HOME/Library/Developer/Xcode/Templates"
 if [ -d "$FILE" ]; then
     echo "$FILE exist"
-	cp -rf "01 Rakuten Templates" "$FILE"
+	cp -rf "ZYCXcodeScaffold" "$FILE"
     echo "copied!"
 else 
     echo "$FILE does not exist"
 	mkdir -p "$FILE"
-	cp -rf "01 Rakuten Templates" "$FILE"
+	cp -rf "ZYCXcodeScaffold" "$FILE"
     echo "copied!"
 fi
